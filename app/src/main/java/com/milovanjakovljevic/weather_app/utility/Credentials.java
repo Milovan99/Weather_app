e3db71f4282d4d26849ed7cebfb2c458e9cd2fd4
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-24 22:27:20 +0100

    Adding credentials for connecting with site open weathermap

---
+++ app/src/main/java/com/milovanjakovljevic/weather_app/utility/Credentials.java
@@ -0,0 +1,9 @@
+package com.milovanjakovljevic.weather_app.utility;
+
+public class Credentials {
+
+    public static final String BASE_URL ="https://openweathermap.org";
+
+    public static final String API_KEY="bee4f3625d4a710dd3fc2263a42c38c6";
+
+}
